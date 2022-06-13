e1adb8d925c1c6fa10ba7a2fab9b66dbd49c52d3
plump_albert <plumpalbert@gmail.com>
2022-06-13 17:15:25 +0300

    stop rebuilding image

--- presentation/pandock.sh
+++ presentation/pandock.sh
@@ -2,10 +2,10 @@
 CWD="$PWD/$(dirname "$0")"
 
 docker rm --force pdp-presentation-speech
-docker build \
-	-t pandoc-cyr \
-	-f "${CWD}/pandoc-cyrillic.Dockerfile" \
-	.
+# docker build \
+# 	-t pandoc-cyr \
+# 	-f "${CWD}/pandoc-cyrillic.Dockerfile" \
+# 	.
 docker run \
 	--detach --rm --pull=never \
 	--name "pdp-presentation-speech" \
@@ -13,6 +13,6 @@ docker run \
 	-v "${CWD}":/data \
 	pandoc-cyr speech.md \
 	--pdf-engine=xelatex \
-	-V 'mainfont:DejaVu Sans' \
+	-V 'mainfont:DejaVu Sans Mono' \
 	-V 'geometry:top=20mm,bottom=20mm,left=25mm,right=20mm' \
 	-o speech.pdf
